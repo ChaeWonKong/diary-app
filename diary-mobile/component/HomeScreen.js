3ce817fdef43961aaa2514b91de37624b334919b
공채원 <chaewonkong@gmail.com>
2018-09-12 15:23:22 +0900

    Add uuid and date to diaryData

--- diary-mobile/component/HomeScreen.js
+++ diary-mobile/component/HomeScreen.js
@@ -12,22 +12,27 @@ import {
 import Header from "./Header";
 import Nav from "./Nav";
 import { Entypo } from "@expo/vector-icons";
+import uuidv1 from "uuid/v1";
 
 const { width, height } = Dimensions.get("window");
 const Opacity = 0.6;
 
 const diaryData = [
   {
-    title: "img1",
-    text: "img1 is...",
+    title: "선인장",
+    text:
+      "선인장은 죽을 때가 되어서야 꽃을 피운다. 그것은 명멸하는 생명력 앞에서 최후 변론을 하는 셈이기도 하다.",
     img: require("../public/images/ex1.png"),
-    id: "0001"
+    date: "2018-01-02",
+    id: ""
   },
   {
-    title: "img2",
-    text: "img2 is...",
+    title: "커피를 마시며",
+    text:
+      "커피는 쓰다. 커피는 향기롭다. 커피향을 맡은 사람은 커피를 찾지만, 정작 커피는 마시면 쓰다. 그것은 어쩌면 멀리서 볼 때는 희극이지만 가까이서 보면 비극인 인생과 닮았는지도 모른다.",
     img: require("../public/images/ex2.png"),
-    id: "0002"
+    date: "2018-09-12",
+    id: ""
   }
 ];
 
@@ -67,7 +72,13 @@ export default class HomeScreen extends Component {
   }
 
   _getDiaries = () => {
-    const diaries = diaryData;
+    const diaries = diaryData.map(diary => {
+      const newDiaries = {
+        ...diary,
+        id: uuidv1()
+      };
+      return newDiaries;
+    });
     this.setState({
       diaries
     });
@@ -80,8 +91,13 @@ export default class HomeScreen extends Component {
           <View style={styles.imagePos}>
             <Image source={diary.img} style={styles.image} />
           </View>
-          <View style={styles.text}>
-            <Text multiline={true}>{diary.text}</Text>
+          <View style={styles.textField}>
+            <Text style={styles.date}>{diary.id}</Text>
+            <Text style={styles.date}>{diary.date}</Text>
+            <Text style={styles.title}>{diary.title}</Text>
+            <Text style={styles.text} multiline={true}>
+              {diary.text}
+            </Text>
           </View>
         </View>
       );
@@ -110,11 +126,21 @@ const styles = StyleSheet.create({
     width: width,
     flex: 1
   },
-  text: {
+  textField: {
     height: (5 * height) / 12,
     justifyContent: "center",
     backgroundColor: "#fff",
     padding: 20
+  },
+  title: {
+    fontSize: 20
+  },
+  date: {
+    fontSize: 16,
+    color: "#bbb"
+  },
+  text: {
+    fontSize: 16
   },
   createButton: {
     position: "absolute",
